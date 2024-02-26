dc3422452f76c2d7feca360dc963819a99a70f44
vijendra2244 <vs230267@gmail.com>
2024-02-26 14:16:37 +0530

    Chat app client side

--- chat_app_server/index.js
+++ chat_app_server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173"],
+    origin: ["http://127.0.0.1:5173","https://chat-app-p3qq.onrender.com"],
     credentials: true,
   })
 );
